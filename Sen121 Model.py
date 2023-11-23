99bf71251dfd152b68f556dc94fd53ae02ce8f6d
apeszko <a.w.peszko@student.tudelft.nl>
2023-11-23 10:23:19 +0100

    Update Sen121 Model.py
    check if it works

--- Sen121 Model.py
+++ Sen121 Model.py
@@ -1,8 +1,13 @@
-pr=float(input("What is you bench press pr (kg): "))
+bench_pr=float(input("What is your bench press pr (kg): "))
+squat_pr=float(input("What is your squat pr (kg): "))
+deadlift_pr=float(input("What is your deadlift pr (kg): "))
+total=bench_pr+squat_pr+deadlift_pr
 
-if pr>=100 and pr<130:
+if (bench_pr>=100 and bench_pr<130) and (squat_pr>=140 and squat_pr<230) and (deadlift_pr>=180 and deadlift_pr<280):
     print("man")
-elif pr>=130:
+elif bench_pr>=130 and squat_pr>=230 and deadlift_pr>=280:
     print("Sam Sulek")
 else:
     print("weak")
+
+print(total)
