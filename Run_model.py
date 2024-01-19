71eff867de036da090c9b487a483fc94c2234bd7
Rikki-driod <r.d.vanrijn@student.tudelft.nl>
2024-01-19 14:43:20 +0100

    REV2 It works

--- Run_model.py
+++ Run_model.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Inizialize the model as you wnat
-model = AdaptationModel(number_of_households=1000, flood_map_choice="100yr", network="watts_strogatz", information_policy_type='NL-Alert',information_policy_radius=8000)
+model = AdaptationModel(number_of_households=1000, flood_map_choice="100yr", network="watts_strogatz",
+                        information_policy_type='NL-Alert', information_policy_radius=8000)
 
 # run 15 steps
 for step in range(15):
@@ -14,27 +15,48 @@ for step in range(15):
 # Put data in data collector
 model_data = model.datacollector.get_model_vars_dataframe()
 
-
-
 print("Number of agents per state per step:")
-print(model_data[['total_households_state_0', 'total_households_state_1', 'total_households_state_2', 'total_households_state_3', 'total_households_state_4']].to_string())
+print(model_data[['total_households_state_0', 'total_households_state_1', 'total_households_state_2',
+                  'total_households_state_3', 'total_households_state_4']].to_string())
 
-params = {"number_of_households": 1000, "information_policy_radius": 8000, "flood_map_choice": ['100yr', '500yr', 'harvey'], "network": ['watts_strogatz', 'erdos_renyi', 'barabasi_albert'], "information_policy_type": ['knocking', 'NL-Alert']}
+params = {"number_of_households": 100, "information_policy_radius": 8000,
+          "flood_map_choice": ['100yr', '500yr', 'harvey'],
+          "network": ['watts_strogatz', 'erdos_renyi', 'barabasi_albert'],
+          "information_policy_type": ['Knocking', 'NL-Alert']}
 
 results = mesa.batch_run(
     AdaptationModel,
     parameters=params,
-    iterations=3,
-    max_steps=45,
+    iterations=1,
+    max_steps=14,
     number_processes=1,
-    data_collection_period=15,
+    data_collection_period=1,
     display_progress=True,
 )
 
-results_df=pd.DataFrame(results)
+results_df=pd.DataFrame(results, columns=['iteration', 'Step', 'flood_map_choice', 'network', 'information_policy_type', 'total_households_state_0', 'total_households_state_1', 'total_households_state_2', 'total_households_state_3', 'total_households_state_4'])
+
 
 print(results_df.keys())
 
-#results_df.to_excel('output.xlsx', index=False)
+#results_df.filter(items=['iteration', 'flood_map_choice', 'network'])
+
+#, 'information_policy_type', 'total_households_state_0', 'total_households_state_1', 'total_households_state_2', 'total_households_state_3', 'total_households_state_4'])
+
+#print(results_df)
+
+#results_filtered = results_df[(results_df.AgentID == 0) & (results_df.Step == 100)]
+#results_filtered[["iteration", "flood_map_choice"]].reset_index(
+ #   drop=True
+#).head()
+
+#print(results_filtered)
+
+results_df.to_excel(r'C:\Users\RDvan\OneDrive\Documents\GitHub\Sen121-Group-13\output.xlsx', index=False)
 
 
+
+# results_df_filtered = results_df[(results_df.AgentID == 0) & (results_df.Step == 15) & (results_df.number_of_households == 1000)]
+# results_df_filtered.head(3)
+
+# print(results_df_filtered)
