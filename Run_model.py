8b6826c6170f85f23d624fb8e74f96876aa1ab5e
Rikki-driod <r.d.vanrijn@student.tudelft.nl>
2024-01-20 18:08:46 +0100

    REV5 - W/ Graph Plot for policies

--- Run_model.py
+++ Run_model.py
@@ -1,8 +1,12 @@
 import mesa
+import seaborn
 
 from model import AdaptationModel
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
+
 
 # Inizialize the model as you wnat
 model = AdaptationModel(number_of_households=1000, flood_map_choice="100yr", network="watts_strogatz",
@@ -19,7 +23,7 @@ print("Number of agents per state per step:")
 print(model_data[['total_households_state_0', 'total_households_state_1', 'total_households_state_2',
                   'total_households_state_3', 'total_households_state_4']].to_string())
 
-params = {"number_of_households": 100, "information_policy_radius": 8000,
+params = {"number_of_households": 1000, "information_policy_radius": 8000,
           "flood_map_choice": ['100yr', '500yr', 'harvey'],
           "network": ['watts_strogatz', 'erdos_renyi', 'barabasi_albert'],
           "information_policy_type": ['Knocking', 'NL-Alert']}
@@ -27,7 +31,7 @@ params = {"number_of_households": 100, "information_policy_radius": 8000,
 results = mesa.batch_run(
     AdaptationModel,
     parameters=params,
-    iterations=1,
+    iterations=3,
     max_steps=14,
     number_processes=1,
     data_collection_period=1,
@@ -57,6 +61,39 @@ print(results_filtered.keys())
 
 results_filtered.to_excel(r'C:\Users\RDvan\OneDrive\Documents\GitHub\Sen121-Group-13\output.xlsx', index=False)
 
+g = sns.lineplot(
+    data=results_filtered,
+    x="Step",
+    y="total_households_state_4",
+    hue="information_policy_type",
+    errorbar=("ci", 95),
+    palette="tab10",
+)
+g.figure.set_size_inches(8, 4)
+plot_title = "Household final state growth over time"
+g.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+plt.show()
+
+#concatenated = pd.concat([results_filtered.total_households_state_0.assign(dataset='set1'), results_filtered.total_households_state_1.assign(dataset='set2'), results_filtered.total_households_state_2.assign(dataset='set3'), results_filtered.total_households_state_3.assign(dataset='set4'), results_filtered.total_households_state_4.assign(dataset='set5')])
+
+#sns.scatterplot(x='iteration', y='Step', data=concatenated)
+
+
+# class v / s fare barplot
+#sns.set_style('whitegrid')
+#sns.barplot(x='iteration', y='total_households_state_3', data=results_filtered)
+
+# Show the plot
+#plt.show()
+
+#seaborn.set(style='whitegrid')
+
+#seaborn.scatterplot(x="Step",
+                    #y="total_households_state_2",
+                   # data=results_filtered)
+
+#plt.show()
 
 
 # results_df_filtered = results_df[(results_df.AgentID == 0) & (results_df.Step == 15) & (results_df.number_of_households == 1000)]
