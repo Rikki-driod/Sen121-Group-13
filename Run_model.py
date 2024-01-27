a3765ffb54b3e95ec775df776afdc7070673509f
Rikki-driod <r.d.vanrijn@student.tudelft.nl>
2024-01-27 12:14:24 +0100

    REV6 - W/ Graph Plot for policies

--- Run_model.py
+++ Run_model.py
@@ -5,6 +5,8 @@ from model import AdaptationModel
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
+
 
 
 
@@ -40,7 +42,7 @@ results = mesa.batch_run(
 
 
 
-results_df=pd.DataFrame(results, columns=['iteration', 'Step', 'AgentID', 'flood_map_choice', 'network', 'information_policy_type', 'total_households_state_0', 'total_households_state_1', 'total_households_state_2', 'total_households_state_3', 'total_households_state_4'])
+results_df=pd.DataFrame(results, columns=['iteration', 'Step', 'AgentID', 'flood_map_choice', 'network', 'information_policy_type', 'total_households_state_0', 'total_households_state_1', 'total_households_state_2', 'total_households_state_3', 'total_households_state_4', 'flood_damage_actual'])
 
 results_filtered = results_df[(results_df.AgentID == 0)]
 
@@ -61,7 +63,7 @@ print(results_filtered.keys())
 
 results_filtered.to_excel(r'C:\Users\RDvan\OneDrive\Documents\GitHub\Sen121-Group-13\output.xlsx', index=False)
 
-g = sns.lineplot(
+g_4 = sns.lineplot(
     data=results_filtered,
     x="Step",
     y="total_households_state_4",
@@ -69,11 +71,72 @@ g = sns.lineplot(
     errorbar=("ci", 95),
     palette="tab10",
 )
-g.figure.set_size_inches(8, 4)
+g_4.figure.set_size_inches(8, 4)
 plot_title = "Household final state growth over time"
-g.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+g_4.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+
+#g_0 = sns.lineplot(
+#    data=results_filtered,
+#    x="Step",
+#    y="total_households_state_0",
+#    hue="information_policy_type",
+#    errorbar=("ci", 95),
+#    palette="tab10",
+#)
+#g_0.figure.set_size_inches(8, 4)
+#plot_title = "Household final state (0) growth over time"
+#g_0.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+#g_1 = sns.lineplot(
+#    data=results_filtered,
+#    x="Step",
+#    y="total_households_state_1",
+#    hue="information_policy_type",
+#    errorbar=("ci", 95),
+#    palette="tab10",
+#)
+#g_1.figure.set_size_inches(8, 4)
+#plot_title = "Household final state (1) growth over time"
+#g_1.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+#g_2 = sns.lineplot(
+#    data=results_filtered,
+#    x="Step",
+#    y="total_households_state_2",
+#    hue="information_policy_type",
+#    errorbar=("ci", 95),
+#    palette="tab10",
+#)
+#g_2.figure.set_size_inches(8, 4)
+#plot_title = "Household final state (2) growth over time"
+#g_2.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+#g_3 = sns.lineplot(
+#    data=results_filtered,
+#    x="Step",
+#    y="total_households_state_3",
+#    hue="information_policy_type",
+#    errorbar=("ci", 95),
+#    palette="tab10",
+#)
+#g_3.figure.set_size_inches(8, 4)
+#plot_title = "Household final state (3) growth over time"
+#g_3.set(title=plot_title, ylabel="Total number of Households", xlabel="Step");
+
+
+
+g = sns.PairGrid(results_filtered, y_vars=["total_households_state_0", "total_households_state_1", "total_households_state_2", "total_households_state_3"], x_vars=["Step"], hue="information_policy_type", height=4)
+g.map(sns.lineplot)
+g.add_legend()
+
+scatter_map = sns.FacetGrid(results_filtered, col="flood_map_choice", height=4)
+scatter_map.map(sns.scatterplot, "iteration", "flood_damage_actual")
+#scatter_map.add_legend()
 
 plt.show()
+
+
 
 #concatenated = pd.concat([results_filtered.total_households_state_0.assign(dataset='set1'), results_filtered.total_households_state_1.assign(dataset='set2'), results_filtered.total_households_state_2.assign(dataset='set3'), results_filtered.total_households_state_3.assign(dataset='set4'), results_filtered.total_households_state_4.assign(dataset='set5')])
 
